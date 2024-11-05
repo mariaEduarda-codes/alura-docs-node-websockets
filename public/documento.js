818abe63ce3452e3f394c1b99e0810c572ea177a
madu <medeiroslima.me@gmail.com>
2024-11-05 20:30:20 -0300

    aula 3

--- public/documento.js
+++ public/documento.js
@@ -1,10 +1,16 @@
-import {emitirTextoEditor} from "./socket-front-documento.js";
+import {emitirTextoEditor, selecionarDocumento} from "./socket-front-documento.js";
 
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get('nome');
 
 const textoEditor = document.getElementById('editor-texto');
+const tituloDocumento = document.getElementById('titulo-documento');
 
-textoEditor.addEventListener('keyup', (e) => {
-    emitirTextoEditor(textoEditor.value);
+tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+selecionarDocumento(nomeDocumento);
+
+textoEditor.addEventListener('keyup', () => {
+    emitirTextoEditor({ texto: textoEditor.value, nomeDocumento });
 });
 
 function atualizaTextoEditor(texto) {
